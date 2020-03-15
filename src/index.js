846b1b0953b70b05cea0ecaaa512d7e0693da2e3
RickMartian <rikmarciano@outlook.com.br>
2020-03-14 21:19:06 -0300

    implementing simple CRUD (simple mutation)

--- src/index.js
+++ src/index.js
@@ -1,25 +1,64 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-const typeDefs = `
-type Query {
-    info: String!
-}
-`
+let links = [
+    {
+        id: "link-0",
+        url: "www.howtographql.com",
+        description: "Fullstack tutorial for GraphQL!"
+    },
+    {
+        id: "link-1",
+        url: "www.google.com.br",
+        description: "Goes to google!!"
+    }
+]
+
+let idCount = links.length;
 
 const resolvers = {
     Query: {
-        info: () => "That's one info"
-    }
+        info: () => "That's one info",
+        feed: () => links
+    },
+    Mutation: {
+        createLink: (parent, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url
+            }
+            links.push(link);
+            return link;
+        },
+        updateLink: (parent, args) => {
+            const foundedLinkId = links.map(link => link.id).indexOf(args.id);
+            if (foundedLinkId !== -1) {
+                links[foundedLinkId] = {
+                    ...links[foundedLinkId],
+                    url: args.url ? args.url : links[foundedLinkId].url,
+                    description: args.description ? args.description : links[foundedLinkId].description
+                };
+                return links[foundedLinkId];
+            }
+        },
+        deleteLink: (parent, args) => {
+            const foundedLinkId = links.map(link => link.id).indexOf(args.id);
+            if (foundedLinkId !== -1) {
+                linkToRemove = links[foundedLinkId];
+                links = links.filter(link => link.id !== args.id);
+                return linkToRemove;
+            }
+        }
+    },
 }
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers,
 });
 
 const options = {
-    port: 4000,
-    endpoint: '/graphql'
+    port: 7777,
 }
 
-server.start(options, ({ port, endpoint }) => console.log(`Server is running at http://localhost:${port}${endpoint}!`))
+server.start(options, ({ port }) => console.log(`Server is running at http://localhost:${port}`))
