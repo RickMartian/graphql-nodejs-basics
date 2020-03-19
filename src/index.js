73e312b43c32be163ac48b0145894f6b2a26cfd8
RickMartian <rikmarciano@outlook.com.br>
2020-03-19 15:44:17 -0300

    adding authentication

--- src/index.js
+++ src/index.js
@@ -1,60 +1,26 @@
 const { GraphQLServer } = require("graphql-yoga");
-
-let links = [
-    {
-        id: "link-0",
-        url: "www.howtographql.com",
-        description: "Fullstack tutorial for GraphQL!"
-    },
-    {
-        id: "link-1",
-        url: "www.google.com.br",
-        description: "Goes to google!!"
-    }
-]
-
-let idCount = links.length;
+const { prisma } = require("./generated/prisma-client");
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const User = require('./resolvers/User');
+const Link = require('./resolvers/Link');
 
 const resolvers = {
-    Query: {
-        info: () => "That's one info",
-        feed: () => links
-    },
-    Mutation: {
-        createLink: (parent, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url
-            }
-            links.push(link);
-            return link;
-        },
-        updateLink: (parent, args) => {
-            const foundedLinkId = links.map(link => link.id).indexOf(args.id);
-            if (foundedLinkId !== -1) {
-                links[foundedLinkId] = {
-                    ...links[foundedLinkId],
-                    url: args.url ? args.url : links[foundedLinkId].url,
-                    description: args.description ? args.description : links[foundedLinkId].description
-                };
-                return links[foundedLinkId];
-            }
-        },
-        deleteLink: (parent, args) => {
-            const foundedLinkId = links.map(link => link.id).indexOf(args.id);
-            if (foundedLinkId !== -1) {
-                linkToRemove = links[foundedLinkId];
-                links = links.filter(link => link.id !== args.id);
-                return linkToRemove;
-            }
-        }
-    },
-}
+    Query,
+    Mutation,
+    User,
+    Link
+};
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
+    context: (request) => {
+        return {
+            ...request,
+            prisma
+        }
+    }
 });
 
 const options = {
